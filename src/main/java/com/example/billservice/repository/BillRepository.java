36a9947a94b02a4ac967f654837466264ac744cc
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-15 16:35:33 +0530

    Modified BillRepository, implemented getBillByPatientsId() operation.

--- src/main/java/com/example/billservice/repository/BillRepository.java
+++ src/main/java/com/example/billservice/repository/BillRepository.java
@@ -2,6 +2,8 @@ package com.example.billservice.repository;
 
 import com.example.billservice.model.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface BillRepository extends JpaRepository<Bill,Long> {
+    List<Bill> findBillsByPatientsId(Long patientId);
 }
