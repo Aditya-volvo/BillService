47b778281a86e34509d97ab1b9c245993fa92f65
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-30 15:17:20 +0530

    Implemented business logic for getBillByPatientId and getAllTransactionByBillId

--- src/main/java/com/example/billservice/service/BillService.java
+++ src/main/java/com/example/billservice/service/BillService.java
@@ -2,6 +2,7 @@ package com.example.billservice.service;
 
 import com.example.billservice.dto.BillRequest;
 import com.example.billservice.dto.BillResponse;
+import com.example.billservice.dto.TransactionResponseDto;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 
@@ -15,4 +16,6 @@ public interface BillService {
     ResponseEntity<BillResponse> getBillById(Long billId);
 
     List<BillResponse> getBillByPatientId(Long patientId);
+
+    List<TransactionResponseDto> getListOfAllTransactionByBillId(Long billId);
 }
--- src/main/java/com/example/billservice/service/serviceimpl/BillServiceImpl.java
+++ src/main/java/com/example/billservice/service/serviceimpl/BillServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.billservice.service.serviceimpl;
 
+import com.example.billservice.client.PatientServiceClient;
+import com.example.billservice.client.TransactionServiceClient;
 import com.example.billservice.dto.BillRequest;
 import com.example.billservice.dto.BillResponse;
+import com.example.billservice.dto.TransactionResponseDto;
 import com.example.billservice.exception.BillNotFoundException;
 import com.example.billservice.mapper.GlobalMapper;
 import com.example.billservice.model.Bill;
@@ -21,6 +24,8 @@ public class BillServiceImpl implements BillService {
     private final BillRepository billRepository;
     private final GlobalMapper globalMapper;
     private final GlobalResponseEntity globalResponseEntity;
+    private final PatientServiceClient patientServiceClient;
+    private final TransactionServiceClient transactionServiceClient;
 
     @Override
     public ResponseEntity<BillResponse> generateBill(BillRequest billRequest) {
@@ -45,10 +50,22 @@ public class BillServiceImpl implements BillService {
 
     @Override
     public List<BillResponse> getBillByPatientId(Long patientId) {
+        // Check if the patient exists in the Patient Service
+        patientServiceClient.getPatientById(patientId);
         List<Bill> bills = billRepository.findBillsByPatientsId(patientId) ;
         if(bills == null){
             throw new BillNotFoundException("Bill with patientId:"+patientId+"not Found");
         }
         return bills.stream().map(globalMapper::mapRepositoryToResponse).toList();
+    }
+
+    @Override
+    public List<TransactionResponseDto> getListOfAllTransactionByBillId(Long billId) {
+        // Check if the bill exists
+        Bill bill = billRepository.findById(billId)
+                .orElseThrow(()-> new BillNotFoundException("Bill with Id:"+billId+"not found"));
+        // Fetch transactions related to the bill
+        List<TransactionResponseDto> transactions = transactionServiceClient.getTransactionsByBillId(billId);
+        return transactions;
     }
 }
