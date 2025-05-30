a10686f999b98453e0d35aec35e769e0f34608b4
Aditya Wadkar <aditya.wadkar@volvo.com>
2025-05-30 15:12:24 +0530

    Created two features getBillByPatientId and getAllTransactionByBillId

--- src/main/java/com/example/billservice/controller/BillController.java
+++ src/main/java/com/example/billservice/controller/BillController.java
@@ -1,6 +1,8 @@
 package com.example.billservice.controller;
+import com.example.billservice.client.TransactionServiceClient;
 import com.example.billservice.dto.BillRequest;
 import com.example.billservice.dto.BillResponse;
+import com.example.billservice.dto.TransactionResponseDto;
 import com.example.billservice.service.BillService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,21 +19,27 @@ public class BillController {
     private final BillService billService;
 
     @PostMapping
-    public ResponseEntity<BillResponse> generateBill(@Valid @RequestBody BillRequest billRequest){
-       return billService.generateBill(billRequest);
+    public ResponseEntity<BillResponse> generateBill(@Valid @RequestBody BillRequest billRequest) {
+        return billService.generateBill(billRequest);
     }
 
     @GetMapping
-    public List<BillResponse> getAllBill(){
+    public List<BillResponse> getAllBill() {
         return billService.getAllBill();
     }
 
     @GetMapping("/{billId}")
-    public ResponseEntity<BillResponse> getBillById(@PathVariable Long billId){
-      return  billService.getBillById(billId);
+    public ResponseEntity<BillResponse> getBillById(@PathVariable Long billId) {
+        return billService.getBillById(billId);
     }
 
-    public List<BillResponse> getBillByPatientsId(@PathVariable Long patientId){
+    @GetMapping("/patient/{patientId}")
+    public List<BillResponse> getBillByPatientsId(@PathVariable Long patientId) {
         return billService.getBillByPatientId(patientId);
+    }
+
+    @GetMapping("/transactions/{billId}")
+    public List<TransactionResponseDto> getListOfAllTransactionByBillId(@PathVariable Long billId) {
+        return billService.getListOfAllTransactionByBillId(billId);
     }
 }
